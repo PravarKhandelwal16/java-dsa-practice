4c83b4701d79bdef33ab327bd59806cf2493dd83
Pravar <pravarkhandelwal47@gmail.com>
2026-03-09 18:54:15 +0530

    StringBuffer

---
+++ Java/Strings/StringBuffer1.java
@@ -0,0 +1,14 @@
+public class StringBuffer1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        System.out.println(sb);
+
+        System.out.println(sb.append(" World"));
+
+        System.out.println(sb.charAt(6));
+
+        System.out.println(sb.delete(6, 10));
+    }
+}
+
+// String Buffer and it's Metods
---
+++ Java/Strings/StringPractise.java
@@ -0,0 +1,15 @@
+public class StringPractise{
+    public static void main(String[] args) {
+        // String Creation;
+        String s = "Hello";
+        System.out.println(s);
+
+        // String Concatenation
+        System.out.println(s + " World");
+
+        // String Methods
+        System.out.println(s.charAt(3));
+        System.out.println(s.concat(" Pravar"));
+        
+    }
+}
